ff69de436010a0818833b0cc75b189f6217bcb8e
Ayormitan <kolawoleabaz97@gmail.com>
2024-04-21 12:45:49 -0700

    Script starts web app

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -3,10 +3,12 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/', strick_slashes=False)
 def hello_hbnb():
     """ Prints a message when / is called """
     return 'Hello HBNB!'
+
 
 @app.route('/hbnb', strick_slashes=False)
 def hbnb():
